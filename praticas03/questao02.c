9efe858cb6f4e87558894acd23b255ec441255c0
Jpezildo <163006817+Jpezildo@users.noreply.github.com>
2024-04-22 13:50:50 +0000

    Aula 07 e Praticas 03

---
+++ praticas03/questao02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  int num100;
+float numero;
+
+  printf("Porcentagem de desconto ");
+  int deu_certo = scanf("%i", &num100);
+
+  if ( num100 == 100.0i) {
+    printf("Entre com a porcentagem de desconto:");
+    deu_certo = scanf("%i", &num100);
+
+
+
+
+
+
+  return 0
+    }
